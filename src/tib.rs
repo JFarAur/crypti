291f87c2454765d73136a208906daab2d1c62859
Jacob Farnsworth <6502enthusiast@gmail.com>
2025-07-19 14:54:26 +0200

    Make TIB constants public

--- src/tib.rs
+++ src/tib.rs
@@ -2,13 +2,13 @@ use iced_x86::Register;
 
 use crate::{mem::{SimMemory, VecMemory}};
 
-const MEM_REGION_TIB: u8 = 0x1u8 << 1;
-const MEM_REGION_PEB: u8 = 0x1u8 << 2;
-const MEM_REGION_LDR: u8 = 0x1u8 << 3;
+pub const MEM_REGION_TIB: u8 = 0x1u8 << 1;
+pub const MEM_REGION_PEB: u8 = 0x1u8 << 2;
+pub const MEM_REGION_LDR: u8 = 0x1u8 << 3;
 
-const TIB_ABSOLUTE_ADDRESS: u64 = 0x1000;
-const PEB_ABSOLUTE_ADDRESS: u64 = 0x3000;
-const LDR_ABSOLUTE_ADDRESS: u64 = 0x4000;
+pub const TIB_ABSOLUTE_ADDRESS: u64 = 0x1000;
+pub const PEB_ABSOLUTE_ADDRESS: u64 = 0x3000;
+pub const LDR_ABSOLUTE_ADDRESS: u64 = 0x4000;
 
 pub fn install_tib(bitness: u32, vecmem: &mut VecMemory) {
     vecmem.mem_mark(Register::None as u64, TIB_ABSOLUTE_ADDRESS as i64, 0x2000, MEM_REGION_TIB);
